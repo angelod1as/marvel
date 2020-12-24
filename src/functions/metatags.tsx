c87c9ace30c784d11b645fd4e776a55378abe047
Angelo Dias <oiangelodias@gmail.com>
2020-12-24 01:28:48 -0300

    Change metatags

--- src/functions/metatags.tsx
+++ src/functions/metatags.tsx
@@ -2,48 +2,39 @@ export default function metatags() {
   return (
     <>
       {/* Primary Meta Tags */}
-      <title>Choose Your Poison, search your drinks!</title>
-      <meta name="title" content="Choose Your Poison, search your drinks!" />
+      <title>mysuperhero</title>
+      <meta name="title" content="mysuperhero" />
       <meta
         name="description"
-        content="Choose your drink and see it's recipe and ingredients"
+        content="Find information about your favourite Marvel character"
       />
       {/* Open Graph / Facebook */}
       <meta property="og:type" content="website" />
-      <meta property="og:url" content="https://chooseyourpoison.vercel.app/" />
-      <meta
-        property="og:title"
-        content="Choose Your Poison, search your drinks!"
-      />
+      <meta property="og:url" content="https://mysuperhero.vercel.app/" />
+      <meta property="og:title" content="mysuperhero" />
       <meta
         property="og:description"
-        content="Choose your drink and see it's recipe and ingredients"
+        content="Find information about your favourite Marvel character"
       />
       <meta
         property="og:image"
-        content="https://media.giphy.com/media/l3fQaE2lbSAPUnB04/giphy.gif"
+        content="https://media.giphy.com/media/eR0GYL4J8UxEI/giphy.gif"
       />
       {/* Twitter */}
       <meta property="twitter:card" content="summary_large_image" />
-      <meta
-        property="twitter:url"
-        content="https://chooseyourpoison.vercel.app/"
-      />
-      <meta
-        property="twitter:title"
-        content="Choose Your Poison, search your drinks!"
-      />
+      <meta property="twitter:url" content="https://mysuperhero.vercel.app/" />
+      <meta property="twitter:title" content="mysuperhero" />
       <meta
         property="twitter:description"
-        content="Choose your drink and see it's recipe and ingredients"
+        content="Find information about your favourite Marvel character"
       />
       <meta
         property="twitter:image"
-        content="https://media.giphy.com/media/l3fQaE2lbSAPUnB04/giphy.gif"
+        content="https://media.giphy.com/media/eR0GYL4J8UxEI/giphy.gif"
       />
-      <meta name="keywords" content="drinks, api, react" />
+      <meta name="keywords" content="marvel, api, react" />
       <meta name="author" content="Angelo Dias <oiangelodias@gmail.com>" />
-      <meta name="url" content="https://chooseyourpoison.vercel.app/" />
+      <meta name="url" content="https://mysuperhero.vercel.app/" />
     </>
   )
 }
